74a452f7922e93b9d12e9f7d2fad8d97ddd9f440
Dipti-khapekar <diptikhapekar0@gmail.com>
2026-06-25 17:12:38 +0530

    update ECommerce.py

--- ECommerce.py
+++ ECommerce.py
@@ -1,11 +1,29 @@
-class Product:
-    def __init__(self,product_id,name,price):
-        self.product_id = product_id
-        self.name = name
-        self.price = price
+from ECommerce import Product
 
-    def display(self):
-        print(f"Product ID : {self.product_id}")
-        print(f"Name : {self.name}")
-        print(f"Price : {self.price}")
-        print("-" * 30)
+class ECommerceSystem:
+
+    def __init__(self):
+        self.producrs = []
+        self.cart = []
+
+    def add_product(self):
+        product_id = int(input("Enter Product ID: "))
+        name = input("Enter your Name: ")
+        price = int(input("Enter Product Price: "))
+
+        product = Product(product_id,name,price)
+        self.products.append(product)
+
+        print("Product added successfully!")
+
+    def display_pproducts(self):
+        pass
+    
+    def search_product(self):
+        product_id = int(input("Enter Product ID: "))
+
+    def add_to_cart(self):
+        product_id = int(input("Enter Product ID: "))
+
+    def view_cart(self):
+        pass
